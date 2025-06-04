ab3c67ab42a42bff4abdd71e9e3e39b80d52c198
v1lling <sascha.villing@web.de>
2025-06-04 12:30:31 +0200

    Add Banner for unapplied changes

---
+++ src/components/unappliedChangesBanner/unappliedChangesBanner.comp.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from '../button/button.comp';
+import './unappliedChangesBanner.scss';
+import { usePageTranslation } from '../../hooks/usePageTranslation';
+
+interface Props {
+  onApply: () => Promise<void>;
+}
+
+export const UnappliedChangesBanner = ({ onApply }: Props) => {
+    const { translate } = usePageTranslation();
+  return (
+    <div className="unapplied-changes-banner">
+      <div className="banner-content">
+        <span className="warning-text">
+          <i className="fa fa-warning" aria-hidden="true"></i>
+          {translate('global.common.unappliedChanges')}
+        </span>
+        <Button onClick={onApply} aria-label={translate('global.aria.applyChanges')}>
+          {translate('global.buttons.applyChanges')}
+        </Button>
+      </div>
+    </div>
+  );
+};
